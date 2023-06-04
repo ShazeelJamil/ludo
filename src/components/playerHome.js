7d96de4a0e68f76f0befdcf977b617053c66687b
ShazeelJamil <shazeeljamil@gmail.com>
2023-06-04 12:01:30 +0500

    using html to move the  div

--- src/components/playerHome.js
+++ src/components/playerHome.js
@@ -10,49 +10,102 @@ export default function PlayerHome(props) {
   };
 
 
-  const [positions, setPositions] = useState([{ x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }]);
+  // const [positions, setPositions] = useState([{ x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }]);
 
-  const playAnimation = (elem, index) => {
-    console.log("Animation");
-    //const targetPawn = document.getElementsByClassName("pawn")[index];
-    const prevX = positions[index].x;
-    const prevY = positions[index].y;
-    const newX = prevX + 100;
-    const newY = prevY + 100;
-    gsap.fromTo(
-      elem,
-      { x: prevX, y: prevY },
-      {
-        x: newX,
-        y: newY,
-        duration: 1,
-      }
-    );
-    const updatedPositions = [...positions];
-    updatedPositions[index] = { x: newX, y: newY };
-    setPositions(updatedPositions);
+  const playAnimation = (event, index, targetDivId) => {
+
+    var targetPawn = event.target;
+    console.log("Target: " + targetDivId);
+    var targetDiv = document.getElementById(targetDivId);
+
+    targetPawn.classList.add("multipleDisplay")
+    if (targetDiv.hasChildNodes()) {
+
+      var lastChild = targetDiv.lastElementChild;
+
+
+      const parentWidth = parentDiv.offsetWidth;
+      const childWidth = childDiv.offsetWidth;
+
+      const leftOffset = (parentWidth - childWidth) / 2;
+      childDiv.style.left = leftOffset + 'px';
+
+
+
+
+
+
+
+
+      var prev_z_index = lastChild.zIndex;
+      var left = lastChild.style.left;
+
+      console.log("last_left: " + lastChild.style.left)
+      console.log("last_zIndex: " + targetDiv.lastElementChild.zIndex)
+
+      targetPawn.zIndex = prev_z_index - 1;
+      targetPawn.style.left = left + 5;
+
+      console.log("new_zIndex: " + targetPawn.zIndex)
+      console.log("new_left: " + targetPawn.style.left)
+
+
+    }
+    else {
+
+      targetPawn.zIndex = 1;
+    }
+
+    targetDiv.appendChild(targetPawn);
+
+
+
+
+
+
+
+
+    // const targetDiv = document.getElementById(targetDivId); // Assuming each div has an ID with the format "div-{index}"
+
+    // const rect = targetDiv.getBoundingClientRect();
+
+    // const prevX = positions[index].x;
+    // const prevY = positions[index].y;
+    // console.log("prev positions", prevX, prevY);
+
+
+    // // const newX = rect.left + (rect.width / 2) - (rect.x / 2);
+    // // const newY = rect.top + (rect.height / 2) - (rect.y / 2);
+
+    // const newX = rect.left;
+    // const newY = rect.top;
+
+    // console.log("new positions", newX, newY);
+
+    // gsap.fromTo(
+    //   targetPawn,
+    //   { x: prevX, y: prevY },
+    //   {
+    //     x: newX,
+    //     y: newY,
+    //     duration: 1,
+    //   }
+    // );
+    // const updatedPositions = [...positions];
+    // updatedPositions[index] = { x: newX, y: newY };
+    // setPositions(updatedPositions);
   };
 
   return (
     <div style={homeStyle}>
       <div className="pawnDiv" style={pawnCSS} >
         {props.pawnArray.map((elem, index) => (
-          <div key={index} onClick={() => playAnimation(props.pawn[index], index)}>
+          <div key={props.bgColor + index} onClick={(e) => playAnimation(e, index, "red01")}>
             {props.pawn}
           </div>
         ))}
       </div>
     </div>
-
-    // <div className='playerHome' style={homeStyle} >
-    //   <div className="pawnDiv" style={pawnCSS} >
-    //     <div key={0} onClick={() => playAnimation(0)}> {props.pawn}</div>
-    //     <div key={1} onClick={() => playAnimation(1)}> {props.pawn}</div>
-    //     <div key={2} onClick={() => playAnimation(2)}> {props.pawn}</div>
-    //     <div key={3} onClick={() => playAnimation(3)}> {props.pawn}</div>
-    //   </div>
-    // </div>
-
 
   )
 }
